b40cfa7cdaac0cc3fa471079741e22d7560d01e1
sanjay-chalise <sanjchalise@gmail.com>
2025-09-18 19:02:56 +0300

    Ass4

---
+++ ass4ex2.py
@@ -0,0 +1,19 @@
+
+names = set()
+
+while True:
+    name = input("Enter a name (or press Enter to finish): ")
+
+    if name == "":
+        break
+
+    if name in names:
+        print("Existing name")
+    else:
+        print("New name")
+        names.add(name)
+
+
+print("\nNames entered:")
+for name in names:
+    print(name)
